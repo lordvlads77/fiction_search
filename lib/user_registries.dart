0492713a8bb6814ea825679bd570453ba01bf95a
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-07-31 15:23:51 -0600

    Started The Login/Registry DB

---
+++ lib/user_registries.dart
@@ -0,0 +1,19 @@
+class UsersRegistry {
+  String? id;
+  String? email;
+  String? username;
+  String? password;
+  String? name;
+  String? favoritefandom;
+
+  UsersRegistry(this.email, this.username, this.password, this.name, this.favoritefandom);
+
+  UsersRegistry.fromJSON(Map<String, dynamic> json){
+    id = json['id'].toString();
+    email = json['email'].toString();
+    username = json['username'].toString();
+    password = json['password'].toString();
+    name = json['name'].toString();
+    favoritefandom = json['favoritefandom'].toString();
+  }
+}
